9d518b8d93109dc4a93d614ce639d22f5f8d495a
pratikbhadane24 <pratikbhadane24@gmail.com>
2022-10-08 18:01:25 +0530

    fixed typo & added likes to storefront app 👜

--- likes/models.py
+++ likes/models.py
@@ -8,4 +8,4 @@ class LikedItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_type = GenericForeignKey()
+    content_object = GenericForeignKey()
